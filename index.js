2b7c223e51aac07207f67a6ec2acf5ad17221bdb
Vinay Chaturvedi <vcvcchaturvedi@gmail.com>
2021-07-17 12:57:31 +0530

    Testing authentication issues

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors({ credentials: true, origin: "https://job-helper.netlify.app/" }));
+app.use(cors({ credentials: true, origin: "https://job-helper.netlify.app" }));
 app.use(cookieParser("mysecretkey"));
 app.use(Express.json());
 app.use(
