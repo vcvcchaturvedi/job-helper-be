cd246a46e5c2b7896ccd5a54eb095c7ef7bcf7fa
Vinay Chaturvedi <vcvcchaturvedi@gmail.com>
2021-07-17 13:43:10 +0530

    Testing authentication issues

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: false,
-      sameSite: "false",
+      sameSite: "none",
       secure: true,
       maxAge: 60 * 60 * 24 * 1000,
     },
