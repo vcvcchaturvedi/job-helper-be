25c97e6061fef0f5e50537508ac4c494b8a299b7
Vinay Chaturvedi <vcvcchaturvedi@gmail.com>
2021-07-17 13:22:21 +0530

    Testing authentication issues

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((user, done) => {
   console.log("Deserializing: " + user);
-  User.find(user.username, (err, user) => {
+  User.findOne({ username: user.username }, (err, user) => {
     if (err) {
       console.log("error: " + err);
       done(null, false, { error: err });
